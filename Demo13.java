83a3b2a5e962fc8f2515a5b9e0e3e41ad393a870
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-06 11:57:06 +0530

    Add Demo13

--- Demo13.java
+++ Demo13.java
@@ -1,3 +1,63 @@
+import java.sql.SQLException;
+
 public class Demo13 {
-    
+
+    static void myMethod1() throws Exception{
+        throw new Exception();
+    }
+
+    // catch hell
+    static void myMethod2(){
+        try {
+            throw new ClassNotFoundException();
+        }catch (ClassNotFoundException e){
+            try {
+                throw e;
+            } catch (ClassNotFoundException e1) {
+                try {
+                    throw e1;
+                } catch (ClassNotFoundException e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static void myMethod3() throws ClassNotFoundException{
+        throw new ClassNotFoundException();
+    }
+
+    static void myMethod4()throws Throwable{
+        throw new Exception();
+    }
+
+    public static void main(String[] args) {
+        try {
+            myMethod5();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch(SQLException e){
+
+        } catch (Throwable t){}
+    }
+
+    static void myMethod5() throws ClassNotFoundException, SQLException{
+        try{
+            if (true) throw new ClassNotFoundException();
+            if (true) throw new SQLException();
+            if (true) throw new RuntimeException();
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    static void myMethod6() throws Exception{
+        try{
+            if (true) throw new ClassNotFoundException();
+            if (true) throw new SQLException();
+            if (true) throw new RuntimeException();
+        }catch (Exception e){
+            throw new Exception(e);
+        }
+    }
 }
